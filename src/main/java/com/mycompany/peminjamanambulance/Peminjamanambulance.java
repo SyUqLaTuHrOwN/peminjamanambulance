3026ac4ea0e62b096da60f9e180bd4cbded99eb2
SyUqLaTuHrOwN <sulthonfawwazfadhlullah@gmail.com>
2026-02-09 08:32:20 +0700

    Initial commit

---
+++ src/main/java/com/mycompany/peminjamanambulance/Peminjamanambulance.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.peminjamanambulance;
+
+/**
+ *
+ * @author MSI
+ */
+public class Peminjamanambulance {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
